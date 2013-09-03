67c550551e92bb1a038e78f20cdbb67625d8c770
Arab Agile <me@arabagile.com>
2013-09-03 13:28:29 +0400

    Basic bootstrap3 template

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="_/css/AGstyle.css" rel="stylesheet" media="screen">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="_/js/html5shiv.js"></script>
+      <script src="_/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/AGscript.js"></script>
+  </body>
+</html>
